906700d376729283d903e24951c62ab93d8fa1d7
Thomas Truong <TruongThomasQ@gmail.com>
2026-03-26 20:59:50 -0700

    refactor: convert app to OOP class and finalize directory section

--- extfix_ui.py
+++ extfix_ui.py
@@ -11,27 +11,42 @@
 
 import customtkinter as ctk
 
+
 ctk.set_appearance_mode("dark")  # Set to dark mode.
 
-app = ctk.CTk()          # Create app.
-app.title("extfix")      # Set the title of the app.
-app.geometry("600x400")  # Set app size.
-app.resizable(0, 0)      # Make app unresizeable.
 
-# Directory selector section.
-directory_selector_frame = ctk.CTkFrame(app, fg_color="transparent", height=50)
-directory_selector_frame.pack(fill="x")
-# Directory label.
-directory_label = ctk.CTkLabel(directory_selector_frame,
-                               text="Target Directory: ", font=("Roboto", 18))
-directory_label.pack(padx=(14, 7), pady=7, side="left")
-# Selected label.
-selected_label = ctk.CTkLabel(directory_selector_frame,
-                              text="No Directory Selected", font=("Roboto", 18),
-                              fg_color=("gray70", "gray30"))
-selected_label.pack(ipadx=10, side="left")
-# Directory selector button.
-btn_directory_selector = ctk.CTkButton(app, text="Select", font=("Roboto", 14))
-btn_directory_selector.pack(padx=14, anchor="w")
+class ExtFixApp(ctk.CTk):
+  def __init__(self):
+    super().__init__()  # Inherit from CTk and initialize.
 
-app.mainloop()
+    self.title("extfix")      # Set the title of the app.
+    self.geometry("600x400")  # Set app size.
+    self.resizable(0, 0)      # Make app unresizeable.
+
+    self.create_directory_section()
+
+
+  def create_directory_section(self):
+    # Directory selector section.
+    dir_frame = ctk.CTkFrame(self, fg_color="transparent")
+    dir_frame.pack(fill="x")
+    # Directory label.
+    dir_label = ctk.CTkLabel(dir_frame, text="Target Directory: ",
+                            font=("Roboto", 18))
+    dir_label.pack(padx=(10, 7), pady=7, side="left")
+    # Selected entry label.
+    selected_entry = ctk.CTkEntry(dir_frame, font=("Roboto", 18),
+                                  border_width=0, corner_radius=0,
+                                  fg_color=("gray70", "gray30"))
+    selected_entry.insert(0, "No Directory Selected")
+    selected_entry.configure(state="readonly")
+    selected_entry.pack(padx=(0, 10), ipadx=10, side="left", fill="x",
+                        expand=True)
+    # Directory selector button.
+    dir_selector_btn = ctk.CTkButton(self, text="Select", font=("Roboto", 14))
+    dir_selector_btn.pack(padx=10, anchor="w")
+
+
+if __name__ == "__main__":
+  app = ExtFixApp()
+  app.mainloop()
