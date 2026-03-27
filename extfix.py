d7b01e8f8ce8053928044c182fc1c534336fa585
Thomas Truong <TruongThomasQ@gmail.com>
2026-03-27 00:00:17 -0700

    refactor: wrapped code in main() for better scope and portability

--- extfix.py
+++ extfix.py
@@ -14,9 +14,8 @@
     NEW_EXT (str): The new extension to replace with.
 """
 
-from pathlib import Path
-
 import sys
+from pathlib import Path
 
 
 DIRECTORY_NAME = "Test"   # The directory path with all the files to be fixed.
@@ -24,15 +23,15 @@ TARGET_EXT = ".jpg"       # The extension you want to be replaced.
 NEW_EXT = ".png"          # The new extension to replace with.
 
 
-if __name__ == "__main__":
+def main():
   # Clean the extensions incase of user-error.
-  TARGET_EXT = TARGET_EXT.lower().strip()
-  NEW_EXT = NEW_EXT.lower().strip()
+  target_ext = TARGET_EXT.lower().strip()
+  new_ext = NEW_EXT.lower().strip()
   # If user forgets the ".", add it for them.
-  if TARGET_EXT and not TARGET_EXT.startswith("."):
-    TARGET_EXT = "." + TARGET_EXT
-  if NEW_EXT and not NEW_EXT.startswith("."):
-    NEW_EXT = "." + NEW_EXT
+  if target_ext and not target_ext.startswith("."):
+    target_ext = "." + target_ext
+  if new_ext and not new_ext.startswith("."):
+    new_ext = "." + new_ext
 
   # Counters
   success_count = 0
@@ -52,20 +51,20 @@ if __name__ == "__main__":
       continue
 
     # File does not have the right extension to convert.
-    if not file.name.endswith(TARGET_EXT):
+    if not file.name.endswith(target_ext):
       skip_count += 1
       print(f"[WARNING] {file.name}: skipped, not the "
-            f"target extension ({TARGET_EXT}).")
+            f"target extension ({target_ext}).")
       continue
 
-    # Edge case: TARGET_EXT == "" is true for everything.
-    if TARGET_EXT == "" and file.suffix != "":
+    # Edge case: target_ext == "" is true for everything.
+    if target_ext == "" and file.suffix != "":
       # Skip file if file already has a suffix.
       continue
 
     # Create new file with the extension.
-    clean_name = file.name.removesuffix(TARGET_EXT)
-    new_file = file.with_name(f"{clean_name}{NEW_EXT}")
+    clean_name = file.name.removesuffix(target_ext)
+    new_file = file.with_name(f"{clean_name}{new_ext}")
 
     # Already a file with the same name using that extension!
     if new_file.exists():
@@ -80,3 +79,7 @@ if __name__ == "__main__":
 
   print(f"Finished converting {success_count} files with "
         f"{skip_count} skipped and {error_count} errors.")
+
+
+if __name__ == "__main__":
+  main()
